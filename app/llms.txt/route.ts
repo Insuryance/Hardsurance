d1b2cda448755d9791fe9ed5e9ae88e78dc02985
Suryansham <isitsuryansham@gmail.com>
2026-09-19 07:15:20 +0530

    Create route.ts for Hardsurance documentation
    Added documentation for Hardsurance including product details and contact information.

---
+++ app/llms.txt/route.ts
@@ -0,0 +1,22 @@
+# Hardsurance
+
+> Insurance intelligence for robotics, drones, data centers and space systems.
+
+Hardsurance maps how physical systems operate, structures insurance policy
+language, tests coverage against real operating scenarios and prepares
+evidence-backed actions for founders, operators and brokers.
+
+## Primary pages
+
+- https://www.hardsurance.com/product
+- https://www.hardsurance.com/demo
+- https://www.hardsurance.com/knowledge
+- https://www.hardsurance.com/use-cases/robotics
+- https://www.hardsurance.com/use-cases/drones
+- https://www.hardsurance.com/use-cases/data-centers
+- https://www.hardsurance.com/use-cases/space
+- https://www.hardsurance.com/insights
+
+## Contact
+
+- contact@hardsurance.com
